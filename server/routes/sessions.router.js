796f8f4378ee2e65087481c62c0bc499a7f2d523
David Trelles <davidstrelles@gmail.com>
2023-02-28 10:24:37 -0600

    added authentication to get route

--- server/routes/sessions.router.js
+++ server/routes/sessions.router.js
@@ -6,16 +6,20 @@ const router = express.Router();
  * GET route template
  */
 router.get("/", (req, res) => {
-  const query = `SELECT * FROM session ORDER BY "date" ASC`;
-  pool
-    .query(query)
-    .then((result) => {
-      res.send(result.rows);
-    })
-    .catch((err) => {
-      console.log("ERROR: Get all sessions", err);
-      res.sendStatus(500);
-    });
+  if (req.isAuthenticated()) {
+    const query = `SELECT * FROM "session" WHERE "user_id" = $1 ORDER BY "date" ASC`;
+    pool
+      .query(query, [req.user.id])
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((err) => {
+        console.log("ERROR: Get all sessions", err);
+        res.sendStatus(500);
+      });
+  } else {
+    res.sendStatus(403);
+  }
   // GET route code here
 });
 
