31f5c3bb6567bad4287a7553b40f67c6a2c3af51
David Trelles <davidstrelles@gmail.com>
2023-02-27 22:10:37 -0600

    added a get request, and a sessions reducer

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  const query = `SELECT * FROM session ORDER BY "date" ASC`;
+  pool
+    .query(query)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR: Get all sessions", err);
+      res.sendStatus(500);
+    });
   // GET route code here
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // POST route code here
 });
 
