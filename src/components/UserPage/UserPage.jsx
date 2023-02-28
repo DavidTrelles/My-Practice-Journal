05ffc0f7cea3b63fa70aff5f688ae5a0c27e8351
David Trelles <davidstrelles@gmail.com>
2023-02-27 19:32:59 -0600

    initial commit, user page started

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import React, { useEffect } from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const sessions = useSelectior((store) => store.sessions);
+  const history = useHistory();
+  useEffect(() => {
+    dispatch({ type: "FETCH_SESSIONS" });
+  }, []);
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <section className="sessions">
+        {sessions.map((session) => {
+          return (
+            <div class="session" key={session.id}>
+              <h3>{session.title}</h3>
+            </div>
+          );
+        })}
+      </section>
+
       <LogOutButton className="btn" />
     </div>
   );
